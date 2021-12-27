cad0d0f6a6ef1f97e00132e5e2f7f948f2ab7e6c
GerardoZuno <gerardozunounitec@gmail.com>
2021-12-27 16:49:30 -0600

    Add appTheme.tsx

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,20 +2,32 @@ import {
   useNavigation,
 } from '@react-navigation/native';
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, Image} from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons'
+import { styles } from '../theme/appTheme';
 
 const HomeScreen = () => {
     const navigation = useNavigation();
+    const {top} = useSafeAreaInsets()
 
   return (
-    <View>
+    <View style={styles.globalMarginal}>
       <Text>HomeScreen</Text>
-      <Icon name="body-outline" size={40} color="red"/>
-      <Button
+      <Image 
+      source={require('../assets/pokebola.png')}
+      style={styles.pokebolaBG}/>
+      {/* <Button
               onPress={() => navigation.navigate('PokemonScreen' as any)}
               title="Pokemon"
-      />
+      /> */}
+      <Text style={{
+          ...styles.titulo,
+          ...styles.globalMarginal,
+          top: top + 20
+      }}>
+          Pokedex
+      </Text>
     </View>
   );
 };
---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+    globalMarginal: {
+        marginHorizontal: 20
+    },
+    pokebolaBG: {
+          width: 300,
+          height: 300,
+          position: 'absolute',
+          top: -100,
+          right:-100,
+          opacity: 0.3
+    },
+    titulo: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        color: 'black'
+    }
+});
