a2043d132020675f774bf1e1d8e89acc8c286985
GerardoZuno <gerardozunounitec@gmail.com>
2021-12-27 16:08:48 -0600

    Add Stack Navigator

---
+++ src/navigator/StackNavigator.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+    >
+      <Stack.Screen
+        name="HomeScreen"
+        options={{
+          title: 'HomeScreen',
+        }}
+        component={HomeScreen}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
---
+++ src/screens/PokemonScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const PokemonScreen = () => {
+    return (
+        <View>
+            <Text>
+                Pokemon
+            </Text>
+            
+        </View>
+    )
+}
+
+export default PokemonScreen
