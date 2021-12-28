5e092e2c2d8feb26aa68783087c03236bbde69ba
GerardoZuno <gerardozunounitec@gmail.com>
2021-12-28 14:30:06 -0600

    Add InfiniteScroll

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -5,20 +5,40 @@ import {pokemonApi} from '../api/pokemonApi';
 import { PokemonPaginatedResponse,  Result,  SimplePokemon } from '../interfaces/pokemonInterfaces';
 
 const usePokemonPaginated = () => {
+    const [isLoading, setisLoading] = useState(true);
+
 
     const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([])
   const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
 
   const loadedPokemons = async () => {
+      setisLoading(true);
     const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
-    console.log(resp.data)
     mapPokemonList(resp.data.results)
     nextPageUrl.current = resp.data.next
   };
 
  const mapPokemonList = (pokemonList: Result[]) => {
-     pokemonList.forEach((pokemon => console.log(pokemon.name )))
- }
+     const newPokemonList: SimplePokemon[] =
+      pokemonList.map(({name, url}) => {
+        const urlParts = url.split('/')
+        const id = urlParts[urlParts.length -2]
+        const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+
+       return {
+           id,
+           picture,
+           name,
+       }
+
+     })
+     setSimplePokemonList([
+         ...simplePokemonList,
+         ...newPokemonList
+
+     ])
+     setisLoading(false)
+    }
 
   useEffect(() => {
     loadedPokemons();
@@ -26,7 +46,9 @@ const usePokemonPaginated = () => {
 
   
   return {
-      setSimplePokemonList
+      simplePokemonList,
+      loadedPokemons,
+      isLoading
   };
 };
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,35 +1,65 @@
-import {
-  useNavigation,
-} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {View, Text, Button, Image} from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import Icon from 'react-native-vector-icons/Ionicons'
+import {View, Text, Button, Image, ActivityIndicator} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
 import usePokemonPaginated from '../hooks/usePokemonPaginated';
-import { styles } from '../theme/appTheme';
+import {styles} from '../theme/appTheme';
 
 const HomeScreen = () => {
-    const navigation = useNavigation();
-    const {top} = useSafeAreaInsets()
-    const {} = usePokemonPaginated()
+  const {top} = useSafeAreaInsets();
+  const {isLoading, simplePokemonList, loadedPokemons} = usePokemonPaginated();
 
   return (
     <View style={styles.globalMarginal}>
       <Text>HomeScreen</Text>
-      <Image 
-      source={require('../assets/pokebola.png')}
-      style={styles.pokebolaBG}/>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={styles.pokebolaBG}
+      />
       {/* <Button
               onPress={() => navigation.navigate('PokemonScreen' as any)}
               title="Pokemon"
       /> */}
-      <Text style={{
+      {/* <Text style={{
           ...styles.titulo,
           ...styles.globalMarginal,
           top: top + 20
       }}>
           Pokedex
-      </Text>
+      </Text> */}
+      <FlatList
+        data={simplePokemonList}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => (
+          <View>
+            <Text style={styles.titulo}>
+              {item.name} - {item.id}
+            </Text>
+            <Image source={{uri: item.picture}}
+             style={{width: 100, height: 100}} />
+          </View>
+        )}
+        keyExtractor={pokemon => pokemon.id}
+        onEndReachedThreshold={0.5}
+        onEndReached={() => {
+          setTimeout(() => {
+            loadedPokemons();
+          }, 1000);
+        }}
+        ListFooterComponent={() => (
+          <ActivityIndicator
+            size={30}
+            color="red"
+            style={{
+              height: 100,
+              width: 100,
+              borderColor: 'blue',
+            }}
+          />
+        )}
+      />
     </View>
   );
 };
